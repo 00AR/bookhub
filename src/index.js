b1545e0d0a21d3a20ecdc743f557d7893e6752c8
Abdul Rafey <abdulrafeyq@gmail.com>
2023-12-15 18:18:15 +0530

    setup context with  openlibrary api for book search

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {
   BrowserRouter, Routes, Route 
 } from 'react-router-dom';
 
+import { AppProvider } from './context';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import BookList from './components/BookList/BookList';
@@ -12,13 +13,15 @@ import BookDetails from './components/BookDetails/BookDetails';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path = "/" element = {<Home />}>
-        <Route path = "about" element = {<About />} />
-        <Route path = "book" element = {<BookList />} />
-        <Route path = "/book/:id" element = {<BookDetails />} />
-        </Route>      
-    </Routes>
-  </BrowserRouter>
+  <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />}>
+          <Route path = "about" element = {<About />} />
+          <Route path = "book" element = {<BookList />} />
+          <Route path = "/book/:id" element = {<BookDetails />} />
+          </Route>      
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
 );
