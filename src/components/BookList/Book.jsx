d5976fab6419a452344a0a65049066604faecffd
Abdul Rafey <abdulrafeyq@gmail.com>
2023-12-16 19:51:50 +0530

    BookDetails component complete

--- src/components/BookList/Book.jsx
+++ src/components/BookList/Book.jsx
@@ -16,7 +16,8 @@ const Book = (book) => {
             </Link>
             <div className="book-item-info-item author fs-15">
                 <span className='text-capitalize fw-7'>Author: </span>
-                <span>{book.author.join(", ")}</span>
+                {/* TODO: what if there are more than one author */}
+                <span>{book.author}</span>
             </div>
 
             <div className="book-item-info-item edition-count fs-15">
